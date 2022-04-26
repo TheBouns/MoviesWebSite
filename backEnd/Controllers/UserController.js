2dc5e68b7bae9ba1101eb85d79ca87d9fd49570a
TheBouns <ilbonsi3@hotmail.com>
2022-04-26 12:02:56 +0200

    implemented bcrypt

--- backEnd/Controllers/UserController.js
+++ backEnd/Controllers/UserController.js
@@ -1,10 +1,12 @@
 const User = require("../Models/User");
+const bcrypt = require("bcryptjs");
 const Movie = require("../Models/Movie");
 
 const UserController = {
   async createUser(req, res) {
     try {
-      const user = await User.create({ ...req.body });
+      const hash = bcrypt.hashSync(req.body.password, 12);
+      const user = await User.create({ ...req.body, password: hash });
       res.status(201).send({ user, message: "user has been created" });
     } catch (error) {
       console.error(error);
